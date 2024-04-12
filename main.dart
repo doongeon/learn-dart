9eb441b61d2195b96729d3c280185ae8a9612107
donggeon Na <ndmb2012@gmail.com>
2024-04-12 15:09:56 +0900

    learn basics of dart

--- main.dart
+++ main.dart
@@ -1,9 +1,43 @@
-void main() {
-  var name = "bull";
+enum Role { Warior, Theif }
 
-  return;
+class Player {
+  late final name;
+  late int health, mana;
+  late String role;
+
+  Player({
+    required this.name,
+    required this.health,
+    required this.mana,
+  });
+
+  Player.createNewPlayer({required name})
+      : this.name = name,
+        health = 100,
+        mana = 100;
+
+  void printStat() {
+    print("user: $name \nhealth: $health / 100 \nmana: $mana / 100");
+  }
+
+  void printRole() {
+    printStat();
+    print("Role: ${role.toString()}\n");
+  }
+
+  void setRole({required role}) {
+    this.role = role;
+  }
 }
 
-void hello() {
-  print("hello");
+void main() {
+  var player = Player.createNewPlayer(name: "Bull");
+
+  player.printStat();
+
+  player.setRole(role: Role.Warior);
+
+  player.printRole();
+
+  return;
 }
